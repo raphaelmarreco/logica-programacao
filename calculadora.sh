47a0d99bd49613e046ad4965b4912b8d28fdefd8
raphaelmarreco <raphael.marreco.08@gmail.com>
2025-01-20 13:43:51 -0300

    Update calculadora.sh
    Mudanças de texto.

--- calculadora.sh
+++ calculadora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Por conta de erros nas depedências do Python3 e PIP, adicionei ao script a rotina de instalação dessas depedências via APT, já que estou utilizando Debian 12
-# Válido também para Ubuntu e outros Debian-based SOs.
+# Válido também para Ubuntu e outros Debian-based OSs.
 
 sudo apt update
 sudo apt upgrade
@@ -8,4 +8,4 @@ sudo apt install python3
 sudo apt install python3-pip
 sudo apt install pipx
 
-python3 /script_calculadora.py
+python3 /PATH/TO/YOUR/script_calculadora.py
